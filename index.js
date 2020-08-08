a3231a233cf7a5c7400db97557953a86f47079fa
Thomas LEDUC <thomaslmoi15@hotmail.fr>
2020-08-08 17:29:17 +0200

    Build build index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var _react = _interopRequireDefault(require("react"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var HoverComponent = function HoverComponent() {
-  return /*#__PURE__*/_react.default.createElement("p", null, "test coucou");
+  return /*#__PURE__*/_react.default.createElement("p", null, "Hover component");
 };
 
 var _default = HoverComponent;
